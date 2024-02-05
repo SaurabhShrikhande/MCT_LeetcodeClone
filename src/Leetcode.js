f508e2a9b2f53032c5c02e495ed11c2563e03133
Saurabh Shrikhande <saurabhshrikhande@200gmail.com>
2024-02-06 02:36:06 +0530

    added css to leetcode

--- src/Leetcode.js
+++ src/Leetcode.js
@@ -8,12 +8,12 @@ export default function Leetcode() {
     return (<div style={{}}>
         <Nav />
         <div style={{ background: "black", border: "20px solid black" }}>
-            <div style={{ background: "white" }}>
+            <div style={{ background: "rgb(40,40,40)" , color:"white"}}>
                 <Split
                     className="split"
                     style={{ padding: "0px", margin: "0px" }}
                 >
-                    <div style={{ minHeight: "calc(100vh - 110px )" }}>Split1</div>
+                    <div style={{ minHeight: "calc(100vh - 150px )", margin:"20px" }}>Split1</div>
                     <div>
                         <div style={{margin:"20px"}}>
                            split2
